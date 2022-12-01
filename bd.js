00ec8cb38008b1686df7437a1cc204b4d96429b3
RomanxAnanev <103737013+RomanxAnanev@users.noreply.github.com>
2022-12-01 19:35:31 +0300

    sec commit

---
+++ bd.js
@@ -0,0 +1,135 @@
+'use strict';
+export const movies = [
+    {
+        id: 1,
+        image: "/img/Rectangle5.png",
+        name:"Tower",
+        genre:"thriller",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2022,
+        rate:3,
+        age:18,
+        video:'https://www.youtube.com/embed/JMFifOyEun4'
+    },
+    {
+        id: 2,
+        image: "/img/панда.jpg",
+        name:"Kung Fu Panda",
+        genre:"action",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2009,
+        rate:4,
+        age:6,
+        video:'https://www.youtube.com/embed/YFdnVvOXTTk'
+    },
+    {
+        id: 3,
+        image: "/img/матрица.jpg",
+        name:"The Matrix",
+        genre:"action",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:1999,
+        rate:5,
+        age:16,
+        video:'https://www.youtube.com/embed/YihPA42fdQ8'
+    },
+    {
+        id: 4,
+        image: "/img/начало.jpg",
+        name:"Inception",
+        genre:"action",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2010,
+        rate:5,
+        age:12,
+        video:'https://www.youtube.com/embed/RlKr_lO45Nc'
+    },
+    {
+        id: 5,
+        image: "/img/ФорестГамп.jpg",
+        name:"Forrest Gump",
+        genre:"drama",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:1994,
+        rate:5,
+        age:12,
+        video:'https://www.youtube.com/embed/otmeAaifX04'
+    },
+    {
+        id: 6,
+        image: "/img/вариор.jpg",
+        name:"Warrioir Nun",
+        genre:"fantasy",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2020,
+        rate:4,
+        age:12,
+        video:'https://www.youtube.com/embed/g4yWWywix_s'
+    },
+    {
+        id: 7,
+        image: "/img/поНаклонной.jpg",
+        name:"Downhill",
+        genre:"drama",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2020,
+        rate:4,
+        age:18,
+        video:'https://www.youtube.com/embed/lEMv-OWDMVo'
+    },
+    {
+        id: 8,
+        image: "/img/Марсианин.jpg",
+        name:"The Martian",
+        genre:"fantasy",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2015,
+        rate:4,
+        age:16,
+        video:'https://www.youtube.com/embed/PnzIxF_Z4oI'
+    },
+    {
+        id: 9,
+        image: "/img/джокер.jpg",
+        name:"Joker",
+        genre:"drama",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2019,
+        rate:5,
+        age:18,
+        video:'https://www.youtube.com/embed/VCMaJLwChfs'
+    },
+    {
+        id: 10,
+        image: "/img/неДыши2.jpg",
+        name:"Dont breathe 2",
+        genre:"thriller",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2021,
+        rate:3,
+        age:18,
+        video:'https://www.youtube.com/embed/_vdzucbM7aQ'
+    },
+    {
+        id: 11,
+        image: "/img/скауты.jpg",
+        name:"Scouts Guide to the Zombie Apocalypse",
+        genre:"horor",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2015,
+        rate:3,
+        age:18,
+        video:'https://www.youtube.com/embed/bRukn_Uv7og'
+    },
+    {
+        id: 12,
+        image: "/img/разрушение.jpg",
+        name:"Demolition",
+        genre:"comedy",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa asperiores possimus velit mollitia aperiam maxime dolor culpa quo ab sed, sapiente est sint fuga quisquam esse nam totam. Fugit, ut?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita explicabo iusto voluptates accusamus voluptatum qui corrupti hic? A placeat libero sit exercitationem corporis reprehenderit adipisci voluptates iusto veniam, corrupti quas?",
+        year:2015,
+        rate:4,
+        age:18,
+        video:'https://www.youtube.com/embed/iRXItnuVEyA'
+    },
+];
